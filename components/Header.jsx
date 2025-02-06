f91df3c3b6cd753e97073078b79081db7e03b485
tswague2000 <hadjaidiatou500@gmail.com>
2025-02-05 22:04:53 -0500

    commmit apres le reglage du responsive du header et l'ajout d'un boutton pour toggle l'affichage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,45 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react"; // Import useState
 import logo from "@/public/logo.svg";
 import styles from "./Header.module.css";
-import hamburger from '@/public/hamburger-button.png'
+import hamburger from '@/public/hamburger-button.png';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // État pour gérer l'ouverture/fermeture du menu
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Bascule l'état du menu
+  };
+
   return (
     <header>
       <div className={styles.navigation}>
         <div className={styles.toggleConatiner}>
-          <button>
+          <button onClick={toggleMenu}>
             <Image className={styles.hamburgerImg} src={hamburger} alt="hamburger-icon" />
           </button>
         </div>
         <div className={styles.logo}>
-          <Link href='/' >
+          <Link href='/'>
             <Image
               className={styles.img}
               src={logo}
-              alt="logo-UnityHoopers" />
+              alt="logo-UnityHoopers"
+            />
           </Link>
         </div>
-        <nav className={styles.navigationBar}>
-          <ul className={`${styles.ul}`}>
-            <li ><Link className={styles.link} href='/'>accueil</Link></li>
-            <li ><Link className={styles.link} href='calendrier'>évenements</Link></li>
-            <li ><Link className={styles.link} href='equipe'>équipe</Link></li>
-            <li ><Link className={styles.link} href='partenaires'>partenaires</Link></li>
+        <nav className={`${styles.navigationBar} ${isMenuOpen ? styles.open : ''}`}>
+          <ul className={styles.ul}>
+            <li><Link className={styles.link} href='/'>accueil</Link></li>
+            <li><Link className={styles.link} href='calendrier'>évenements</Link></li>
+            <li><Link className={styles.link} href='equipe'>équipe</Link></li>
+            <li><Link className={styles.link} href='partenaires'>partenaires</Link></li>
           </ul>
         </nav>
       </div>
     </header>
   );
 }
-
-
-
-
-
