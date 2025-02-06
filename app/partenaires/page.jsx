83f826c87c097d4bb14adadb230c7cdcffc727f0
SamzyNendji <2726220@collegelacite.ca>
2025-02-06 13:05:44 -0500

    ajout du composant sponsors et utilisation

--- app/partenaires/page.jsx
+++ app/partenaires/page.jsx
@@ -1,6 +1,12 @@
+// Importation du composant Sponsors depuis le répertoire components
+import Sponsors from '../../components/Sponsors';
 
-export default function partenaires (){
-    return <>
-    page partenaires
-    </>
+export default function Page() {
+  return (
+    // Conteneur principal de la page
+    <div>
+      {/* Inclusion du composant Sponsors */}
+      <Sponsors />
+    </div>
+  );
 }
--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,7 +1,36 @@
-import styles from'@/components/Sponsors.module.css';
+// Importation du composant Image de la librairie "next/image"
+import Image from "next/image";
 
-export default function Sponsors (){
-    return <>
-    composant sponsors
-    </>
+// Importation des styles CSS pour le composant Sponsors
+import styles from "./Sponsors.module.css";
+
+// Importation des images des sponsors
+import Becker from "@/public/logopartenaires/Becker.jpeg";
+import Burger_King from "@/public/logopartenaires/Burger_King.jpg";
+import Coca from "@/public/logopartenaires/Coca.jpeg";
+import Hibou from "@/public/logopartenaires/Hibou.jpeg";
+import Ottawa from "@/public/logopartenaires/Ottawa.jpeg";
+import RBC from "@/public/logopartenaires/RBC.jpeg";
+import Mamba from "@/public/logopartenaires/Mamba.jpg";
+import Cite from "@/public/logopartenaires/Cite.jpg";
+import NBA_Image from "@/public/logopartenaires/NBA_Image.jpg";
+
+// Fonction principale du composant Sponsors
+export default function Sponsors() {
+  return (
+    // Conteneur principal pour le composant Sponsors avec les styles appliqués
+    <div className={styles.container}>
+      <div className={styles.sponsorsList}>
+        <Image className={styles.sponsorImage} src={Becker} alt="Becker"/>
+        <Image className={styles.sponsorImage} src={Burger_King} alt="Burger_King"/>
+        <Image className={styles.sponsorImage} src={Coca} alt="Coca"/>
+        <Image className={styles.sponsorImage} src={Ottawa} alt="Ottawa"/>
+        <Image className={styles.sponsorImage} src={RBC} alt="RBC"/>
+        <Image className={styles.sponsorImage} src={Hibou} alt="Hibou"/>
+        <Image className={styles.sponsorImage} src={Cite} alt="Cite"/>
+        <Image className={styles.sponsorImage} src={NBA_Image} alt="NBA_Image"/>
+        <Image className={styles.sponsorImage} src={Mamba} alt="Mamba"/>
+      </div>
+    </div>
+  );
 }
